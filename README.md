a15484cabb3e70dbb72dfb1c2ed167097fb57160
Gaurav Chaudhary <gaurav.chaudhary@ntnu.no>
2023-10-19 00:04:50 +0200

    first commit

--- README.md
+++ README.md
@@ -14,12 +14,12 @@ To help you plan the parameters of the learning rate policy, a Jupyter Notebook 
 - **steps_per_epoch (int):** Number of steps per epoch. Is equal to number of training samples divided by batch size. = ceil(num_samples / batch_size)
 - **first_lr_drop_mult (float):** Drop factor for learning rate after the first warmup.
 - **general_lr_decay (float):** Decay factor for learning rate.
-- **if_warmup_cooldown_start (int):** 0 for warmup start, 0 for cooldown start. Decide whether to start with warmup or cooldown step.
+- **if_warmup_or_cooldown_start (int):** 0 for warmup start, 0 for cooldown start. Decide whether to start with warmup or cooldown step.
 - **init_cooldown_length (int):** Initial number of epochs in a cooldown step.
 - **init_cooldown_mult_factor (float):** Factor to grow the cooldown step length.
 - **warmup_length (int):** Number of epochs in warmup period.
 - **warmup_mult_factor (float):** Factor to grow the warmup length.
-- **if_no_post_warmup (int):** 0 for no post warmup, 1 for post warmup. Decide whether to have post warmup step, i.e. after first warmup.
+- **if_post_warmup (int):** 0 for no post warmup, 1 for post warmup. Decide whether to have post warmup step, i.e. after first warmup.
 - **number_of_cooldowns_before_switch (int):** Epoch to switch to a new cooldown length.
 - **new_cooldown_length (int):** New cooldown length after switch epoch.
 - **new_cooldown_mult_factor (float):** Factor to grow the new cooldown length.
@@ -37,80 +37,80 @@ To help you plan the parameters of the learning rate policy, a Jupyter Notebook 
 ### 1. Cosine annealing with initial warmup, decaying max-lr and increasing warmup length. Fixed length for cycles.
 ```python
 sgdr = SGDRScheduler_custom(
-    min_lr=0.0001, 
+    min_lr=0.001, 
     max_lr=0.01, 
-    steps_per_epoch=68,
+    steps_per_epoch=10,
     first_lr_drop_mult=1,
     general_lr_decay=0.95, 
-    if_warmup_cooldown_start=0,
+    if_warmup_or_cooldown_start=0,
     init_cooldown_length=30,
     init_cooldown_mult_factor=1.1,
     warmup_length=10,
     warmup_mult_factor=1.2,
-    if_no_post_warmup=1,
+    if_post_warmup=1,
     number_of_cooldowns_before_switch=0,
     new_cooldown_length=30,
     new_cooldown_mult_factor=1.1,
     )
 ```
-![example1](./TEST/LRS_1.png "example1")
+![example1](./TEST/LRS-1.png "example1")
 ### 2. Cosine annealing without initial warmup and constant max-lr. Fixed length for cycles.
 ```python
 sgdr = SGDRScheduler_custom(
-    min_lr=0.0001, 
+    min_lr=0.001, 
     max_lr=0.01, 
-    steps_per_epoch=68,
+    steps_per_epoch=10,
     first_lr_drop_mult=1,
     general_lr_decay=1, 
-    if_warmup_cooldown_start=1,
+    if_warmup_or_cooldown_start=1,
     init_cooldown_length=30,
     init_cooldown_mult_factor=1,
     warmup_length=10,
     warmup_mult_factor=1,
-    if_no_post_warmup=0,
+    if_post_warmup=0,
     number_of_cooldowns_before_switch=0,
     new_cooldown_length=30,
     new_cooldown_mult_factor=1,
     )
 ```
-![example2](./TEST/LRS_2.png "example2")
+![example2](./TEST/LRS-2.png "example2")
 ### 3. Cosine annealing with initial warmup, 5 initial small cycles and decaying max-lr. Fixed length for initial small cycles and increasing length for followup cycles.
 ```python
 sgdr = SGDRScheduler_custom(
-    min_lr=0.0001, 
+    min_lr=0.001, 
     max_lr=0.01, 
-    steps_per_epoch=68,
+    steps_per_epoch=10,
     first_lr_drop_mult=1,
     general_lr_decay=0.95, 
-    if_warmup_cooldown_start=0,
+    if_warmup_or_cooldown_start=0,
     init_cooldown_length=20,
     init_cooldown_mult_factor=1,
     warmup_length=10,
     warmup_mult_factor=1,
-    if_no_post_warmup=0,
+    if_post_warmup=0,
     number_of_cooldowns_before_switch=5,
     new_cooldown_length=40,
     new_cooldown_mult_factor=1.2,
     )
 ```
-![example3](./TEST/LRS_3.png "example3")
+![example3](./TEST/LRS-3.png "example3")
 ### 4. Cosine annealing with initial warmup, increasing warmup length, 5 initial small cycles and decaying max-lr. Fixed length for initial small cycles and increasing length for followup cycles. First cycle has 70% reduced max-lr.
 ```python
 sgdr = SGDRScheduler_custom(
-    min_lr=0.0001, 
+    min_lr=0.001, 
     max_lr=0.01, 
-    steps_per_epoch=68,
+    steps_per_epoch=10,
     first_lr_drop_mult=0.70,
     general_lr_decay=0.95, 
-    if_warmup_cooldown_start=0,
+    if_warmup_or_cooldown_start=0,
     init_cooldown_length=20,
     init_cooldown_mult_factor=1,
     warmup_length=5,
     warmup_mult_factor=1.2,
-    if_no_post_warmup=1,
+    if_post_warmup=1,
     number_of_cooldowns_before_switch=5,
     new_cooldown_length=40,
     new_cooldown_mult_factor=1.2,
     )
 ```
-![example4](./TEST/LRS_4.png "example4")
+![example4](./TEST/LRS-4.png "example4")
--- src/SGDR_custom.py
+++ src/SGDR_custom.py
@@ -17,14 +17,14 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
         first_lr_drop_mult (float): Drop factor for learning rate after the first warmup.
         general_lr_decay (float): Decay factor for learning rate.
 
-        if_warmup_cooldown_start (int): 0 for warmup start, 1 for cooldown start. Decide whether to start with warmup or cooldown step.
+        if_warmup_or_cooldown_start (int): 0 for warmup start, 1 for cooldown start. Decide whether to start with warmup or cooldown step.
 
         init_cooldown_length (int): Initial number of epochs in a cooldown step.
         init_cooldown_mult_factor (float): Factor to grow the cooldown step length.
 
         warmup_length (int): Number of epochs in warmup period.
         warmup_mult_factor (float): Factor to grow the warmup length.
-        if_no_post_warmup (int): 0 for no post warmup, 1 for post warmup. 
+        if_post_warmup (int): 0 for no post warmup, 1 for post warmup. 
                                 Decide whether to have post warmup step, i.e. after first warmup.
 
 
@@ -49,12 +49,12 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
                  first_lr_drop_mult,
                  steps_per_epoch,
                  general_lr_decay,
-                 if_warmup_cooldown_start,
+                 if_warmup_or_cooldown_start,
                  init_cooldown_length,
                  init_cooldown_mult_factor,
                  warmup_length,
                  warmup_mult_factor,
-                 if_no_post_warmup,
+                 if_post_warmup,
                  number_of_cooldowns_before_switch,
                  new_cooldown_length,
                  new_cooldown_mult_factor,
@@ -67,14 +67,14 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
         self.first_lr_drop_mult = first_lr_drop_mult
         self.general_lr_decay = general_lr_decay
 
-        self.if_warmup_cooldown_start = if_warmup_cooldown_start
+        self.if_warmup_or_cooldown_start = if_warmup_or_cooldown_start
 
         self.init_cooldown_length = init_cooldown_length
         self.init_cooldown_mult_factor = init_cooldown_mult_factor
 
         self.warmup_length = warmup_length
         self.warmup_mult_factor = warmup_mult_factor
-        self.if_no_post_warmup = if_no_post_warmup
+        self.if_post_warmup = if_post_warmup
 
         self.number_of_cooldowns_before_switch = number_of_cooldowns_before_switch
         self.new_cooldown_length = new_cooldown_length
@@ -84,7 +84,7 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
 
         self.batch_since_restart = 0
         self.batch_since_warmup = 0
-        self.warmup_cycle_mode = self.if_warmup_cooldown_start    #warmup =0, cooldown=1
+        self.warmup_cycle_mode = self.if_warmup_or_cooldown_start    #warmup =0, cooldown=1
         
         if self.warmup_cycle_mode==0:                   #warmup
             self.next_restart = warmup_length
@@ -100,7 +100,7 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
 
     def cosine_cooldown_clr(self):
         '''Calculate the learning rate cosine fall.'''
-        fraction_to_restart = self.batch_since_restart / (self.steps_per_epoch * self.init_cooldown_length)
+        fraction_to_restart = (self.batch_since_restart+1) / (self.steps_per_epoch * self.init_cooldown_length)
         lr = self.min_lr + 0.5 * (self.max_lr - self.min_lr) * (1 + np.cos(fraction_to_restart * np.pi))
         #lr = self.min_lr + (self.max_lr - self.min_lr) * (1-fraction_to_restart)
         if self.verbose == 1:
@@ -109,7 +109,7 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
     
     def warmup_clr(self):
         '''Calculate the learning rate during warmup.'''
-        fraction_to_restart = self.batch_since_warmup / (self.steps_per_epoch * self.warmup_length)
+        fraction_to_restart = (self.batch_since_warmup+1) / (self.steps_per_epoch * self.warmup_length)
         lr = self.min_lr + (self.max_lr - self.min_lr) * fraction_to_restart
         if self.verbose == 1:
             print('[TrainClass] warmup_clr - fraction_to_restart-', fraction_to_restart)
@@ -124,7 +124,7 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
         '''Record previous batch statistics and update the learning rate.'''
         if self.warmup_cycle_mode==0:
             K.set_value(self.model.optimizer.lr, self.warmup_clr()) 
-            self.batch_since_warmup += 1     
+            self.batch_since_warmup += 1  
         if self.warmup_cycle_mode==1:
             K.set_value(self.model.optimizer.lr, self.cosine_cooldown_clr())
             self.batch_since_restart += 1
@@ -135,6 +135,7 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
             self.history.setdefault(k, []).append(v)
 
     def on_epoch_end(self, epoch, logs={}):
+
         '''Check for end of current cycle, apply restarts when necessary.'''
         if epoch+1 == self.next_restart:
             self.warmup_cycle_mode = not self.warmup_cycle_mode # swap 0 to 1 and vice versa
@@ -142,14 +143,39 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
             self.batch_since_restart = 0
             self.batch_since_warmup = 0
             
+            if self.if_warmup_or_cooldown_start == 0:
+                if self.warmup_cycle_mode==0: 
+                    if self.cycle_step_flag == 1:                              
+                        self.max_lr = self.max_lr * self.first_lr_drop_mult   # drop lr after first warmup or cooldown mode
+                        if self.if_post_warmup == 0:
+                            self.warmup_length = 1 
+                            self.warmup_mult_factor = 1
+                    else:
+                        self.max_lr *= self.general_lr_decay
+
+            if self.if_warmup_or_cooldown_start == 1:
+                if self.warmup_cycle_mode==0: 
+                    if self.cycle_step_flag == 0:                              
+                        self.max_lr = self.max_lr * self.first_lr_drop_mult   # drop lr after first warmup or cooldown mode
+                        if self.if_post_warmup == 0:
+                            self.warmup_length = 1 
+                            self.warmup_mult_factor = 1
+                    else:
+                        self.max_lr *= self.general_lr_decay
+            
             if self.warmup_cycle_mode==0: 
+                if self.verbose == 1:
+                    print('Warmup length: %s' % self.warmup_length)
+                    print('Warmup mult factor: %s' % self.warmup_mult_factor)
                 self.warmup_length = np.ceil(self.warmup_length * self.warmup_mult_factor)
                 self.next_restart += self.warmup_length
                 if self.verbose == 1:
                     print('[TrainClass] Warmup restart at epoch %s, lr-' % (epoch+1), K.get_value(self.model.optimizer.lr))
 
             if self.warmup_cycle_mode==1: 
-                # increase cycle length after certain warmup-cycle mode
+                if self.verbose == 1:
+                    print('Cooldown length: %s' % self.init_cooldown_length)
+                    print('Cooldown mult factor: %s' % self.init_cooldown_mult_factor)
                 if self.flagswitch == self.number_of_cooldowns_before_switch:
                     self.init_cooldown_length = self.new_cooldown_length
                     self.init_cooldown_mult_factor = self.new_cooldown_mult_factor
@@ -157,25 +183,10 @@ class SGDRScheduler_custom(Callback):       #modified to have warmup every resta
                 self.init_cooldown_length = np.ceil(self.init_cooldown_length * self.init_cooldown_mult_factor)
                 self.next_restart += self.init_cooldown_length
                 self.flagswitch = self.flagswitch + 1
+                self.cycle_step_flag = self.cycle_step_flag + 1 
                 if self.verbose == 1:
                     print('[TrainClass] Cycle restart at epoch %s, lr-' % (epoch+1), K.get_value(self.model.optimizer.lr))
         
-        if epoch+1 == self.next_restart-1:
-            self.cycle_step_flag = self.cycle_step_flag + 1                    # counts the number of warmup or cooldown mode
-            
-            if self.if_warmup_cooldown_start == 1:
-                check_mode = 1
-            if self.if_warmup_cooldown_start == 0:
-                check_mode = 2
-
-            # decide when to drop lr after first warmup or cooldown mode
-            if self.cycle_step_flag == check_mode:                              
-                self.max_lr = self.max_lr * self.first_lr_drop_mult   # drop lr after first warmup or cooldown mode
-                if self.if_no_post_warmup == 0:
-                    self.warmup_length = 1 
-                    self.warmup_mult_factor = 1
-            else:
-                self.max_lr *= self.general_lr_decay                       # general_lr_decay after 2nd warmup or cooldown mode
-        
+                
         logs = logs or {}
         logs['lr'] = K.get_value(self.model.optimizer.lr)  
